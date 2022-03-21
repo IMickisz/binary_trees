f544b1e7167b3b968d1a4562c36fabf167149026
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-03-21 12:42:05 +0100

    added task 1 and 2 and updated header file with all mandatory tasks prototypes

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,22 +10,19 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 
-	binary_tree_t *new_node, *tmp;
+	binary_tree_t *tmp;
 
 	if (parent == NULL)
 		return NULL;
 
 	if (parent->left == NULL)
-	{
-		new_node = binary_tree_node(parent->left, value);
-		parent->left = new_node;
-	}
+		parent->left = binary_tree_node(parent, value);
 	else
 	{
 		tmp = parent->left;
-		new_node = binary_tree_node(parent->left, value);
-		new_node->left = tmp;
-		new_node->left->parent = new_node;
+		parent->left = binary_tree_node(parent, value);
+		tmp->parent = parent->left;
+		parent->left->left = tmp;
 	}
-	return new_node;
+	return (parent->left);
 }
